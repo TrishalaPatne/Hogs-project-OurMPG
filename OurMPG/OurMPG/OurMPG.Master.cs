f3ccf680030a7eefdc7258d13f3a3b8622d8c42f
Rekha <reks.rekha@gmail.com>
2016-10-07 10:40:53 -0500

    Clean up template

---
+++ OurMPG/OurMPG/OurMPG.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace OurMPG
+{
+    public partial class OurMPG : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- OurMPG/OurMPG/OurMPG/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace OurMPG.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
