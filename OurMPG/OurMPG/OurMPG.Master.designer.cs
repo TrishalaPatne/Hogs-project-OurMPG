ee99e57b016c17df3697d7ec5f66678bdfda04ad
TrishalaPatne <trishalapatne@gmail.com>
2016-11-04 15:07:09 -0500

    Created Admin Reports (#12)
    * Created Admin Reports
    
    * Integration changes

--- OurMPG/OurMPG/OurMPG.Master.designer.cs
+++ OurMPG/OurMPG/OurMPG.Master.designer.cs
@@ -76,6 +76,15 @@ namespace OurMPG {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl listUserReports;
         
         /// <summary>
+        /// listAdminReports control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl listAdminReports;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
